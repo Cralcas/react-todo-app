9a16c10302fc5e0388c0bd1a492850f553ca2599
Cralcas <carlosbergq@gmail.com>
2024-08-17 22:10:52 +0200

    remove button

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,31 @@
+import { useContext } from "react";
 import { Todo } from "../models/Todo";
+import { TodosDispatchContext } from "../contexts/TodoContext";
+import { ActionType } from "../reducers/TodoReducer";
 
 interface TodoItemProps {
   todo: Todo;
 }
 
 export const TodoItem = ({ todo }: TodoItemProps) => {
+  const dispatch = useContext(TodosDispatchContext);
+
+  if (!dispatch) {
+    throw new Error("dispatch is not defined");
+  }
+  const removeTodo = (id: number) => {
+    dispatch({
+      type: ActionType.REMOVED,
+      payload: id.toString(),
+    });
+  };
+
   return (
     <>
       <div>
         <span>{todo.text}</span>
         <button>Toggle</button>
-        <button>Delete</button>
+        <button onClick={() => removeTodo(todo.id)}>X</button>
       </div>
     </>
   );
--- src/reducers/TodoReducer.ts
+++ src/reducers/TodoReducer.ts
@@ -7,12 +7,16 @@ export interface IAction {
 
 export enum ActionType {
   ADDED,
+  REMOVED,
 }
 
 export const TodoReducer = (todos: Todo[], action: IAction): Todo[] => {
   switch (action.type) {
     case ActionType.ADDED:
       return [...todos, new Todo(action.payload)];
+
+    case ActionType.REMOVED:
+      return todos.filter((todo) => todo.id !== +action.payload);
 
     default:
       return todos;
