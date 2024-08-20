85a11899f4bee630337df426f2e59bb4535e416f
Cralcas <carlosbergq@gmail.com>
2024-08-20 12:54:23 +0200

    todoItem text + buttons

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -28,16 +28,16 @@ export const TodoItem = ({todo}: TodoItemProps) => {
     <>
       <li className="flex mb-3 text-lg break-all">
         <p
-          className={`w-full p-1 font-Bebas border-b ${
+          className={`flex-grow p-1 text-xl font-Bebas border-b ${
             todo.done ? "text-gray-500 line-through" : "text-black"
           } `}
         >
           {todo.text}
         </p>
-        <div className="flex gap-2.5 items-center">
+        <div>
           <button
             className={`${
-              todo.done ? "bg-gray-500" : "bg-green-700"
+              todo.done ? "bg-gray-500" : "bg-green-700 mr-1"
             } px-1 rounded-sm text-white font-sm font-bold`}
             onClick={() => toggleTodo(todo.id)}
           >
